4576149ec5ba0415cd2cee325b78b97d27c65ace
lalawila <wangosky@gmail.com>
2017-03-22 16:05:17 +0800

    test

--- index.php
+++ index.php
@@ -11,6 +11,6 @@ if (!defined('ABSPATH')) {
 //echo '</br>';
 //echo var_dump ($_REQUEST);
 
-
-require_once (ABSPATH . 'setting.php');
-require_once (ABSPATH . 'display-page.php');
+echo 'ess';
+//require_once (ABSPATH . 'setting.php');
+//require_once (ABSPATH . 'display-page.php');
