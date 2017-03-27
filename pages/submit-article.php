5e7f57370101d29cb0a198281e46721f5c39770b
lalawila <wangosky@gmail.com>
2017-03-27 09:24:02 +0800

    fixed bug

--- pages/submit-article.php
+++ pages/submit-article.php
@@ -1,7 +1,9 @@
-<?php get_header(); ?>
+<?php get_header(); 
+if( empty($_POST['title']) || empty($_POST['author']) || empty($_POST['article']) ):
+?>
 <main id="main" class="site-main" role="main"
 <div class="submit-article">
-    <form class="sumbit-article-form" name="sumbit-article-form" id="sumbit-article-form" action="<?php echo get_site_url() ?>/submit-article" method="post">
+    <form class="sumbit-article-form" name="sumbit-article-form" id="sumbit-article-form" action="/submit-article" method="post">
 	<input type="text" name="title" id="article-title"  placeholder="article titlee"></input>
 	<input type="text" name="author" id="article-author"  placeholder="article author"></input>
 	<textarea id = "article" name = "article"  placeholder="article"></textarea>
@@ -12,3 +14,9 @@
     </form>
 </div>
 </main>
+<?php
+else:
+	echo $_POST['title'] . $_POST['author'] . $_POST['article'];
+	global $db;
+	$db->insert('');
+endif;
